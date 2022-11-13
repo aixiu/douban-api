3feea601157de78daf28ae508d96584c976cf7bc
aixiu <4815563@qq.com>
2022-11-14 00:48:10 +0800

    init

--- api/index.py
+++ api/index.py
@@ -12,13 +12,12 @@ import json
 app = FastAPI()
 
 @app.get("/api")
-def news():
-    # 解决浏览器中json数据，中文无法展示的问题
-    app.config['JSON_AS_ASCII']=False   # 中文正常化，解决乱码
-    app.config['JSON_SORT_KEYS']=False   # json不排序
-    app.config['JSONIFY_PRETTYPRINT_REGULAR']=True  # 输出json格式化完美显示
-    app.config["JSONIFY_MIMETYPE"] = "application/json;charset=utf-8"   # 指定浏览器渲染的文件类型，和解码格式；
-    return 'Hello, world !!'
+def news(response: Response):
+    response.headers["Cache-Control"] = "max-age=86400, immutable, stale-while-revalidate"
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    
+    return 'hello'
 
 
 if __name__ == "__main__":
