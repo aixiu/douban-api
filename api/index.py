c9dc0817f0947b70a7a155e974d4f8e3d7d306b3
aixiu <4815563@qq.com>
2022-11-14 00:57:47 +0800

    init

--- api/index.py
+++ api/index.py
@@ -3,18 +3,14 @@
 # @Author: Aixiu
 # @Time  : 2022/11/14 00:34:37
 
-import uvicorn
-from fastapi import FastAPI, Response
-from api.crawler import main as new
-import json
+from flask import Flask
+
+app = Flask(__name__)
 
 
-app = FastAPI()
+@app.route('/')
+def hello():
+    return 'Hello, world !!'
 
-@app.get("/api")
-def news():   
-    return 'hello'
-
-
-if __name__ == "__main__":
-    uvicorn.run("index:app", host="127.0.0.1", port=62, log_level="info")
+if __name__ == '__main__':
+    app.run()
