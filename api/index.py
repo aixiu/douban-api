db5f7e35b3613dab72dae8e5c30e1a12b26906fa
aixiu <4815563@qq.com>
2022-11-14 13:12:54 +0800

    init

--- api/index.py
+++ api/index.py
@@ -32,6 +32,101 @@ def index():
             'date': datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
         }
         return jsonify(error_data)
+    url = f'https://movie.douban.com/subject/{vod_douban_id}/'
+    
+    flie_path = f'./douban'
+    
+    # 创建一个文件夹，保存所有的图片
+    if not os.path.exists(flie_path):
+        os.mkdir('./douban/')
+    
+    # 获取文件夹下所有文件名
+    file_name_list = os.listdir(flie_path)    
+
+
+    #判读豆瓣ID文件是否存在如果存在，直接读文件，如果不存在爬取数据并保存
+    if f'{vod_douban_id}.json' in file_name_list:
+        with open(f'{flie_path}/{vod_douban_id}.json', mode='r', encoding='utf-8') as fp:
+            json_data = json.load(fp=fp)            
+        return jsonify(json_data)   
+        # jsonify将字典转为json,并返回给前端content-type：application/json
+    else:
+        try:
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53'
+            }
+            page_text = requests.post(url=url, headers=headers, timeout=20)
+            page_text_data = requests.post(url=url, headers=headers, timeout=20)
+            page_text_data.raise_for_status()
+            page_text_data.encoding = page_text_data.apparent_encoding
+            page_text = page_text_data.text
+            
+            vod_name = api.dbconfig.vod_name(page_text=page_text)
+            vod_year = api.dbconfig.vod_year(page_text=page_text)
+            vod_lang = api.dbconfig.vod_lang(page_text=page_text)
+            vod_sub = api.dbconfig.vod_sub(page_text=page_text)
+            vod_pic = api.dbconfig.vod_pic(page_text=page_text)
+            vod_class = api.dbconfig.vod_class(page_text=page_text)
+            vod_actor = api.dbconfig.vod_actor(page_text=page_text)
+            vod_writer = api.dbconfig.vod_writer(page_text=page_text)
+            vod_score = api.dbconfig.vod_score(page_text=page_text)                
+            vod_content = api.dbconfig.vod_content(page_text=page_text)
+            vod_area = api.dbconfig.vod_area(page_text=page_text)
+            vod_director = api.dbconfig.vod_director(page_text=page_text)
+            vod_pubdate = api.dbconfig.vod_pubdate(page_text=page_text)
+            vod_duration = api.dbconfig.vod_duration(page_text=page_text)
+            vod_remarks = api.dbconfig.vod_remarks(page_text=page_text)
+    
+            
+            # 国语或英文中字
+            # if '中国' or '台湾' in vod_area:
+            #     vod_remarks = "高清国语"   
+            # else:
+            #     vod_remarks = "高清中字"            
+            
+            vod_total =  vod_remarks
+            vod_score_num =  random.randint(100, 1000)
+            vod_score_all =  random.randint(200, 500)
+            vod_douban_score =  vod_score        
+            vod_reurl = url   # 豆瓣地址
+            
+            vod_data = {
+                "vod_name": vod_name,
+                "vod_sub": vod_sub,
+                "vod_pic": vod_pic,
+                "vod_year": vod_year,
+                "vod_lang": vod_lang,
+                "vod_class": vod_class,
+                "vod_actor": vod_actor,
+                "vod_content": vod_content,
+                "vod_writer": vod_writer,
+                "vod_area": vod_area,
+                "vod_remarks": vod_remarks,
+                "vod_director": vod_director,
+                "vod_pubdate": vod_pubdate,
+                "vod_total": vod_total,
+                "vod_score": vod_score,
+                "vod_douban_score": vod_douban_score,
+                "vod_score_num": vod_score_num,
+                "vod_score_all": vod_score_all,
+                "vod_duration": vod_duration,
+                "vod_reurl": vod_reurl,
+                "vod_douban_id": vod_douban_id,        
+            }        
+    
+            with open(f'{flie_path}/{vod_douban_id}.json', mode='w', encoding='utf-8') as fp:
+                json.dump(vod_data, fp=fp, ensure_ascii=False, indent=4)
+            
+            return jsonify(vod_data)
+            
+        except Exception :
+            error_data = {
+                'code': 404,
+                'msg': 'ID不合法',
+                'info': '一切有为法，如梦幻泡影，如露亦如电，当作如是观。',
+                'date': datetime.now().strftime(r"%Y-%m-%d %H:%M:%S")
+            }
+            return jsonify(error_data)
 
 @app.route('/test')
 def test():
