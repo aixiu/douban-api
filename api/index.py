6499e3de28ad8708d6d3d76e2fe41f5dc785e8f5
aixiu <4815563@qq.com>
2022-11-14 13:06:45 +0800

    init

--- api/index.py
+++ api/index.py
@@ -14,11 +14,7 @@ import dbconfig
 
 app = Flask(__name__)
 
-# 解决浏览器中json数据，中文无法展示的问题
-app.config['JSON_AS_ASCII']=False   # 中文正常化，解决乱码
-app.config['JSON_SORT_KEYS']=False   # json不排序
-app.config['JSONIFY_PRETTYPRINT_REGULAR']=True  # 输出json格式化完美显示
-app.config["JSONIFY_MIMETYPE"] = "application/json;charset=utf-8"   # 指定浏览器渲染的文件类型，和解码格式；
+
 
 @app.route('/')
 def index():
